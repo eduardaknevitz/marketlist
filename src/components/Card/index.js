f837090ed1da6177fa00b60f17b03c45a74f60a7
Eduarda <eduarda.knevitz@hotmail.com>
2023-11-19 13:20:37 -0300

    Cards e ajuste de css no banner

---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import styles from "./Card.module.css";
+
+function Card({ id, titulo, capa }) {
+  return (
+    <div className={styles.container}>
+      <img src={capa} alt={titulo} className={styles.capa} />
+      <h2>{titulo}</h2>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 
@@ -9,8 +10,13 @@ function Inicio() {
       <Cabecalho />
       <Banner imagem="home" />
       <Titulo>
-        <h1>Produtos</h1>
+        <h1>Todos produtos</h1>
       </Titulo>
+      <Card
+        id="1"
+        titulo="produto 1"
+        capa="https://ser.vitao.com.br/wp-content/uploads/2017/12/shutterstock_252338818-1-920x535.jpg"
+      />
       <Rodape />
     </>
   );
