2582895f1a7ea2465f9e601f520be53a3677f4a2
Eduarda <eduarda.knevitz@hotmail.com>
2023-11-19 12:16:19 -0300

    Banner

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,12 @@
 import Cabecalho from "components/Cabecalho";
+import Banner from "components/Banner";
 import Rodape from "components/Rodape";
 
 function Inicio() {
   return (
     <>
       <Cabecalho />
+      <Banner imagem="home" />
       <Rodape />;
     </>
   );
